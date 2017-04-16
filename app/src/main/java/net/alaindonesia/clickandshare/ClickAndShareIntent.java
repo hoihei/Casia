1b5970b61e92c4fb64698c582120890d36eb950c
None <None>
2017-04-16 15:48:24 +0800

    adding forgetten java

---
+++ app/src/main/java/net/alaindonesia/clickandshare/ClickAndShareIntent.java
@@ -0,0 +1,34 @@
+package net.alaindonesia.clickandshare;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class ClickAndShareIntent extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+        String uriString = "";
+
+        if (uri != null)
+            uriString = uri.toString();
+        else
+            finish();
+
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/plain");
+        i.putExtra(Intent.EXTRA_SUBJECT, "Share/Send/Open the link");
+        i.putExtra(Intent.EXTRA_TEXT, uriString);
+        startActivity(Intent.createChooser(i, "Share/Send/Open the link"));
+
+        finish();
+
+
+    }
+
+}
---
+++ app/src/main/java/net/alaindonesia/clickandshare/ClickAndShareLauncher.java
@@ -0,0 +1,26 @@
+package net.alaindonesia.clickandshare;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+public class ClickAndShareLauncher extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Toast.makeText(this.getApplicationContext(),
+                "Nothing to shown in here. This just an app launcher. " +
+                        "\nJust click a web link in other app." +
+                        "\nThen share/send/open it in any app you want",
+                Toast.LENGTH_LONG).show();
+        finish();
+
+    }
+
+
+}
+
+
