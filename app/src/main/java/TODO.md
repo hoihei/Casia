39b66fc5f9d331d003f924d9f22a27e67520cf84
Riyanto Jayadi <riyanto.jayadi@gmail.com>
2017-08-11 22:56:05 +0800

    does not work

---
+++ app/src/main/java/TODO.md
@@ -0,0 +1,3 @@
+- TODO: Configuration activity for
+    * Click needed for casual browsing
+    * Time to start focus
--- app/src/main/java/net/alaindonesia/caslia/WebViewer.java
+++ app/src/main/java/net/alaindonesia/caslia/WebViewer.java
@@ -11,33 +11,66 @@ import android.widget.Toast;
 
 public class WebViewer extends Activity {
 
+    WebView webView;
+
+    final private int FOCUS_TIME_MAX = 20; // seconds
+    final private int CLICK_NEED_MAX = 20;
+
+    private int click_countdown = 1;
+    private String repeatUriString = "";
+    private boolean is_start_focus_countdown;
+    private boolean is_focus_time;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        webView = (WebView) findViewById(R.id.webView);
 
         Intent intent = getIntent();
         String action = intent.getAction();
         String type = intent.getType();
 
+        is_start_focus_countdown = false;
+        is_focus_time = false;
+
         if (Intent.ACTION_SEND.equals(action) && type != null) {
             if ("text/plain".equals(type)) {
                 String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
                     if (sharedText != null) {
-                        loadUrl(sharedText);
+//                        loadUrl(sharedText);
+
+                        webView.loadUrl(sharedText);
                     }
-                }
             }
+        }
+
+
+//        int start_focus_time_countdown = 0;
+//        while (start_focus_time_countdown <= FOCUS_TIME_MAX){
+//            if (is_start_focus_countdown){
+//
+//                try {
+//                    TimeUnit.SECONDS.sleep(1);
+//                } catch (InterruptedException ignored) {
+//                }
+//
+//                start_focus_time_countdown +=1;
+//                Toast.makeText(webView.getContext(), "Focus on " + start_focus_time_countdown + "/"
+//                        + FOCUS_TIME_MAX, Toast.LENGTH_SHORT).show();
+//            }
+//
+//        }
+//
+//        is_focus_time = true;
+//        webView.getSettings().setJavaScriptEnabled(false);
 
     }
 
-    private int countDown=1;
-    private int MAX_COUNT_DOWN=20;
-    private String repeatUriString = "";
 
     @SuppressLint("SetJavaScriptEnabled")
     private void loadUrl(final String uriString){
-        final WebView webView = (WebView) findViewById(R.id.webView);
+
 
         webView.getSettings().setSupportZoom(true);
         webView.getSettings().setJavaScriptEnabled(true);
@@ -48,32 +81,37 @@ public class WebViewer extends Activity {
 
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String uriString) {
-                Toast.makeText(view.getContext(), "No casual browsing " + countDown + "/" + MAX_COUNT_DOWN, Toast.LENGTH_SHORT).show();
+//                if (is_focus_time) {
+                    Toast.makeText(view.getContext(), "Focus time, click more " + click_countdown + "/" + CLICK_NEED_MAX, Toast.LENGTH_SHORT).show();
 
-                if (countDown >= MAX_COUNT_DOWN){
-                    Uri uri = Uri.parse(uriString);
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
-                    countDown = 1;
-                }
+                    if (click_countdown >= CLICK_NEED_MAX) {
+                        Uri uri = Uri.parse(uriString);
+                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(intent);
+                        click_countdown = 1;
+                    }
 
-                if (repeatUriString.equals(uriString))
-                    countDown = countDown + 1;
-                else
-                    countDown = 1;
+                    if (repeatUriString.equals(uriString))
+                        click_countdown = click_countdown + 1;
+                    else
+                        click_countdown = 1;
                     repeatUriString = uriString;
 
-                return true;
+                    return true;
+//                }else{
+//                    return false;
+//                }
             }
 
             @Override
             public void onPageFinished(WebView view, String url){
-                view.getSettings().setJavaScriptEnabled(false);
+                is_start_focus_countdown = true;
+
             }
         });
 
-
-        webView.loadUrl(uriString);
+//
+//        webView.loadUrl(uriString);
     }
 
     @Override
